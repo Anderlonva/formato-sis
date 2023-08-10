6cd8a5ea4f5673480998bc1170a53883ad3b856e
Anderson Londoño <84043589+Anderlonva@users.noreply.github.com>
2023-08-10 17:42:55 -0500

    modified inputs calificacion :lipstick::lipstick:

--- src/components/ui/Calificacion.js
+++ src/components/ui/Calificacion.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Calificacion = () => {
     return (
         <>
-            <div className='row mb-3 mx-2 '>
+            <div className='row mb-3 mx-2 mt-4'>
                 <div className='col-lg-3 '>
                     <div className='row mt-4'>
                         <div className='col-lg-3 text-center'>
@@ -26,33 +26,94 @@ export const Calificacion = () => {
                 </div>
 
 
-                <div className='col-lg-8 bg-gris'>
+                <div className='col-lg-9 col-md-8 bg-gris'>
 
                     <div className='row mt-2'>
                         <h6><strong>¡SOLO PARA SIS DE JURADO!</strong></h6>
                     </div>
 
                     <div className='row mb-2'>
-                        <div className='col-5 '>
-                            <span className='jurado'><strong>Creatividad</strong></span><br />
-                            <span className='jurado'><strong>Participación en la implementación</strong></span><br />
-                            <span className='jurado'><strong>Relación con el cargo</strong></span><br />
-                            <span className='jurado'><strong>Aportes</strong></span><br />
-                            <span className='jurado'><strong>Transversabilidad</strong></span><br />
-                            <span className='jurado'><strong>Total</strong></span><br />
-                        </div>
-                        <div className='col-3'>
-                            <span className='jurado'><strong>Calificación Jurado</strong></span><br />
-                            <br />
-                            <span className='jurado'><strong>Firma Jurado</strong></span><br />
-                            <br />
-                            <span className='jurado'><strong>Nombre del jurado</strong></span><br />
-                        </div>
-                        <div className='col-2 '>
-                            <div className='col'>
-                                <input type="text" readOnly class="form-control mt-1" id='caJurado' />
+
+                        <div className='col-7 '>
+                            <div className='row'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Creatividad</strong></span>
+                                </div>
+                                <div className='col-lg-2'>
+                                    <input type="text" readOnly class="inputJurado" id='creatividad' />
+                                </div>
+                            </div>
+                            <div className='row mt-1'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Part. en la implementación</strong></span>
+                                </div>
+                                <div className='col-1'>
+                                    <input type="text" readOnly class="inputJurado" id='partImplementacion' />
+                                </div>
+                            </div>
+                            <div className='row mt-1'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Relación con el cargo</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <input type="text" readOnly class="inputJurado" id='relacionCargo' />
+                                </div>
+                            </div>
+
+                            <div className='row mt-1'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Aportes</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <input type="text" readOnly class="inputJurado" id='aportes' />
+                                </div>
+                            </div>
+                            <div className='row mt-1'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Transversabilidad</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <input type="text" readOnly class="inputJurado" id='transversabilidad' />
+                                </div>
+                            </div>
+                            <div className='row mt-1'>
+                                <div className='col-lg-6'>
+                                    <span className='jurado'><strong>Total</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <input type="text" readOnly class="inputJurado" id='total' />
+                                </div>
                             </div>
                         </div>
+
+                        <div className='col-5'>
+                            <div className='row mt-1'>
+                                <div className='col-lg-7'>
+                                    <span className='jurado'><strong>Calificación Jurado</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <input type="text" readOnly class="inputJurado" id='calificacionJurado' />
+                                </div>
+                            </div>
+                            <div className='row mt-4'>
+                                <div className='col-lg-7'>
+                                    <span className='jurado'><strong>Firma Jurado</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                <span>_______________</span>
+                                </div>
+                            </div>
+
+                            <div className='row mt-4'>
+                                <div className='col-lg-7'>
+                                    <span className='jurado'><strong>Nombre del Jurado</strong></span>
+                                </div>
+                                <div className='col-lg-1'>
+                                    <span>_______________</span>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
@@ -60,3 +121,9 @@ export const Calificacion = () => {
         </>
     )
 }
+
+/*
+    
+<input type="text" readOnly class="inputJurado2" id='firmaJurado' />
+<input type="text" readOnly class="inputJurado2" id='nombreJurado' />
+*/
